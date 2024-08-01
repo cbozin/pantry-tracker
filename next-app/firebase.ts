8b78f4f1443d73854ba1fd280739c4a595fe1081
[Carolyn Bozin] <[carolynbozhinov@gmail.com]>
2024-08-01 12:26:06 -0700

    removed extra files

--- next-app/firebase.ts
+++ next-app/firebase.ts
@@ -17,6 +17,17 @@ const firebaseConfig = {
   measurementId: "G-RG9CL5RXR8"
 };
 
+// const firebaseConfig = {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APP_ID,
+//   measurementId: process.env.MEASUREMENT_ID
+// };
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
